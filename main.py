c6923609c7671e0743f1a7cf97a9e12310e07501
lebo <lehaichau.22@gmail.com>
2020-01-13 17:28:34 +0700

    update

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ ser = serial.Serial(
 
 
 # print(ser.isOpen())
-# ser.write('ate0\r\n'.encode())
+ser.write('ate0\r\n'.encode())
 
 # msg='at\r\n'
 # ser.write('ate0\r\n'.encode())
@@ -30,7 +30,9 @@ ser = serial.Serial(
 # 	print(type(out))
 
 
-def print_respone():
+
+
+def respone():
 	sleep(1)
 	out = ''
 	while ser.inWaiting() > 0:
@@ -38,23 +40,45 @@ def print_respone():
 		# print (">>" + out)
 	if out != '':
 		# out=out.replace(msg,'')
-		print (out)
+		# print (out)
 		# print(sys.getsizeof(out)-sys.getsizeof(msg))
 		# print(type(out))
 		return out
 
 #check
 #if ok return OK, if not return ERROR
-def check():
-	ser.write('AT+CPIN?\r\n'.encode())
+def test_command(cmd):
+	ser.write('AT\r\n'.encode())
 	sleep(1)
 	out = ''
 	while ser.inWaiting() > 0:
 		out += ser.read(1).decode("utf-8")
 		# print (">>" + out)
 	if out != '':
-		return str(out)
+		# return str(out)
+		if "OK" in out:
+			return True
+		elif "ERROR" in out:
+			return False
 
+
+#============================GENERAL====================
+# Show name network
+# TODO: Detail the ERROR
+# TODO: Detail the OK
+def name_network():
+	if test_command("at+cops=?"):
+		ser.write('AT+COPS?\r\n'.encode())
+		res=respone()
+		return res
+	return "Error"
+
+
+def signal_quality():
+	# if test_command("at+csq=?")
+	ser.write('AT+CSQ\r\n'.encode())
+	res=respone()
+	return res
 
 
 #====================================SMS===============================
@@ -62,16 +86,23 @@ def check():
 # string: phoneno: phone number destination
 # string: msg to send
 def send_sms(phoneno, msg):
-	ser.write('AT+CMGF=1\r\n'.encode()) #Configuring TEXT mode
-	print_respone()
-	ser.write(("AT+CMGS=\"" + phoneno + "\"\n").encode())	#chr(26)= Ctrl+Z
-	sleep(0.5)
-	ser.write((msg+chr(26)).encode())
-	print_respone()
+	if test_command("at+cmgs=?"):
+		ser.write('AT+CMGF=1\r\n'.encode()) #Configuring TEXT mode
+		respone()
+		ser.write(("AT+CMGS=\"" + phoneno + "\"\n").encode())	#chr(26)= Ctrl+Z
+		sleep(0.5)
+		ser.write((msg+chr(26)).encode())
+		print(respone())
+		return "Sent"
+	return "Fail vcl"
 
 def recv_sms():
 	ser.write('AT+CMGR=1\r\n'.encode())
-	print_respone()
+	respone()
+#~~~ERROR: Not working~~~
+def read_sms(slot):		# slot in sim card
+	ser.write(('AT+CMGR='+str(slot)+'\r\n').encode())
+	return(respone())
 	
 
 #======================================PIN==================================
@@ -87,31 +118,38 @@ def pin_code(code):
 	return switcher.get(code,"Invalid code")
 
 def checkpin():
-	ser.write('AT+CPIN?\r\n'.encode())
-	code=print_respone()
-	# print(code[9:-8])
-	# print(pin_code(code[9:-8]));
-	return pin_code(code[9:-8])
+	if test_command("at+cpin=?"):
+		ser.write('AT+CPIN?\r\n'.encode())
+		code=respone()
+		print(code)
+		# print(code[9:-8])
+		# print(pin_code(code[9:-8]));
+		return pin_code(code[9:-8])
+	return "cannot excute"
 
 	
 
 #===================================CALL=================================
+
+
 def voice_call(phoneno):
-	ser.write(("atd"+phoneno).encode())
-	print_respone()
+	ser.write(("atd"+phoneno+";\r\n").encode())
+	print(respone())
 	
 
 
 
 #=========================================+MAIN==============================
 if __name__ == '__main__':
-	# ser.write('AT+CPIN=1279\r\n'.encode())
+	ser.write('AT+COPS?\r\n'.encode())
+	print(respone())
 	# print(send_sms("0886885207","hello"))
 	# print(checkpin())
 	# print(check())
 	# code = '+CPIN: SIM PIN\r\n'
 	# print(type(code[7:-2]))
-	# print(voice_call("0886885207"))
+	print(voice_call("0886885207"))
 	# print(chr(65))
-	recv_sms()
+	print(name_network())
+	# print(signal_quality())
 	ser.close()
